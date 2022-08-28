15b0fc2280f636580d371080da81e9764cb41cc5
MatteoGenovese <matteo.genovese@icloud.com>
2022-08-28 16:31:06 +0200

    refactor: 'all genre' select added statically in the html. the genre is now display in relation with the data passed from the controller.php

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 const app = new Vue({
     el: '#app',
     data: {
-        dataStoraged: false,
         cards: [],
         cardsFiltered: [],
         genreList: [],
-        selectedGenre: "all",
+        selectedMusicGenre: "all genres",
 
     },
     methods: {
@@ -14,17 +13,15 @@ const app = new Vue({
                 .then((response) => {
                     this.cards = response.data.data;
                     console.log(response.data.data);
-                    if (this.dataStoraged == false) {
-                        this.searchGenreToDisplay();
-                    }
-                    this.dataStoraged = true;
+                    this.searchGenreToDisplay();
                 })
                 .catch((error) => {
                     console.warn(error);
                 });
         },
+
         searchGenreToDisplay: function() {
-            this.genreList.push('all')
+            this.genreList = [];
             this.cards.forEach(card => {
                 if (!(this.genreList.includes(card.genre.toLowerCase())))
                     this.genreList.push(card.genre.toLowerCase());
